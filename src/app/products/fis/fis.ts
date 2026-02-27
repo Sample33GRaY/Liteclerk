ea27f2446f65b017b3b1a6a9ed66ca4642082d01
Sample33GRaY <jiovannesam.clarus@gmail.com>
2026-02-27 14:04:26 +0800

    Updated Pictures for cloud fis and his, added balance sheets,  income statement, and cash flow in cloud fis product. Added 2payslips and government contribution in cloud his product. (to be enhanced)

--- src/app/products/fis/fis.ts
+++ src/app/products/fis/fis.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { Cta } from "../../components/cta/cta";
+import { CommonModule } from '@angular/common';
 
 
 @Component({
   selector: 'app-fis',
-  imports: [Cta],
+  imports: [Cta, CommonModule],
   templateUrl: './fis.html',
   styleUrl: './fis.css',
 })
 export class Fis {
+  selectedImage: string | null = null;
 
+  openImageModal(imageSrc: string) {
+    this.selectedImage = imageSrc;
+  }
+
+  closeImageModal() {
+    this.selectedImage = null;
+  }
 }
--- src/app/products/his/his.ts
+++ src/app/products/his/his.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
 import { Cta } from "../../components/cta/cta";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-his',
-  imports: [Cta],
+  imports: [Cta, CommonModule],
   templateUrl: './his.html',
   styleUrl: './his.css',
 })
 export class His {
+  currentSlide = 0;
+  slides = [
+    {
+      title: 'PAYSLIP',
+      image: '/Images/cloudhis/Payslip_Sample2.png',
+      alt: 'Cloud HIS Payroll Dashboard'
+    },
+    {
+      title: 'PAYSLIP',
+      image: '/Images/cloudhis/Payslip_Sample.png',
+      alt: 'Cloud HIS Payroll Dashboard'
+    },
+    {
+      title: 'GOVERNMENT CONTRIBUTIONS',
+      image: '/Images/cloudhis/Government.png',
+      alt: 'Cloud HIS Government Contributions'
+    }
+  ];
 
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide = (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
 }
